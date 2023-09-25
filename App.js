8d7a40e948ba4f49ea0c578a12d6d058d4fc3bf2
Mustansar Hussain <75372985+mustansarRehmti@users.noreply.github.com>
2023-09-25 10:43:52 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+// import MainNavigation from "./navigation/MainNavigation"
+import StackNavigator from './navigation/StackNavigator';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import MapNav from './map/MapNav';
+// import MapInterface from './map/MapInterface';
+// import MapMain from './map/MapMain';
+// import BookingNavMain from "./map/BookingNavMain"
+
+// const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <StackNavigator/>
+   
+  )
+}
+
+export default App
